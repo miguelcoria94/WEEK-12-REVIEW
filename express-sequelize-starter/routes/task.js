922c0714d5ce37e907457354355d32c57c8b84d4
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-01 10:21:05 -0600

    added get route

--- express-sequelize-starter/routes/task.js
+++ express-sequelize-starter/routes/task.js
@@ -4,6 +4,49 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/models");
 
+const { check, validationResult } = require("express-validator");
+
+const validateTask = [
+  //  Task name cannot be empty:
+  check("name")
+    .exists({ checkFalsy: true })
+    .withMessage("Task name can't be empty."),
+  //  Task name cannot be longer than 255 characters:
+  check("name")
+    .isLength({ max: 255 })
+    .withMessage("Task name can't be longer than 255 characters."),
+];
+
+const handleValidationErrors = (req, res, next) => {
+  const validationErrors = validationResult(req);
+
+  // If the validation errors are empty,
+  if (!validationErrors.isEmpty()) {
+    // Generate an array of error messages
+    const errors = validationErrors.array().map((error) => error.msg);
+
+    // Generate a new `400 Bad request.` Error object
+    // and invoke the next function passing in `err`
+    // to pass control to the global error handler.
+    const err = Error("Bad request.");
+    err.status = 400;
+    err.title = "Bad request.";
+    err.errors = errors;
+    return next(err);
+  }
+
+  // Invoke the next middleware function
+  next();
+};
+
+const taskNotFoundError = (id) => {
+  const err = Error(`Task with id of ${id} could not be found.`);
+  err.title = "Task not found.";
+  err.status = 404;
+  return err;
+};
+
+
 const asyncHandler = (handler) => (req, res, next) =>
   handler(req, res, next).catch(next);
 
@@ -20,11 +63,30 @@ router.get(
 
 router.post(
     "/",
+    validateTask,
+    handleValidationErrors,
     asyncHandler(async (req, res) => {
         const { name } = req.body
         const task = await Task.create({ name })
         res.status(201).json({task})
     })
 )
+
+router.get(
+  "/:id(\\d+)",
+  asyncHandler(async (req, res, next) => {
+    const taskId = parseInt(req.params.id, 10);
+    const task = await Task.findByPk(taskId);
+
+    if (task) {
+      res.json({ task });
+    } else {
+      next(taskNotFoundError(taskId));
+    }
+  })
+);
+
+
+
 
 module.exports = router;
